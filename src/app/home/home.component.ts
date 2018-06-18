581a52206f5e60862347ff5eebddb8e37eb8c303
willys_tjiawi <willys@huubap.com>
2018-06-18 11:18:41 +0800

    interval simple example with ngOnDestroy to avoid mem leak

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import 'rxjs/Rx';
-import {Observable} from 'rxjs/observable';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
     const myNum = Observable.interval(1000);
 
-    myNum.subscribe(
+    this.subscription = myNum.subscribe(
       (num: number) => {
         console.log(num);
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
