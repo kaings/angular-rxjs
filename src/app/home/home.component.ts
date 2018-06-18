d18f4ab15e59ce22ef9f15131ab41dc832031f71
willys_tjiawi <willys@huubap.com>
2018-06-18 12:18:04 +0800

    rxjs-observable-create example

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import 'rxjs/Rx';
 import {Observable} from 'rxjs/Observable';
 import {Subscription} from 'rxjs/Subscription';
+import {Observer} from 'rxjs/Observer';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +11,13 @@ import {Subscription} from 'rxjs/Subscription';
 })
 export class HomeComponent implements OnInit, OnDestroy {
   subscription: Subscription;
+  subscription2: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+
+    /* Observable Interval */
     const myNum = Observable.interval(1000);
 
     this.subscription = myNum.subscribe(
@@ -21,10 +25,47 @@ export class HomeComponent implements OnInit, OnDestroy {
         console.log(num);
       }
     );
+
+    /* Observable Create */
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first batch');
+        }, 2000);
+      setTimeout(() => {
+        observer.next('second batch');
+      }, 4000);
+      setTimeout(() => {
+        // observer.error('error occurs');  // it is either error or complete.. choose one. They cannot coexist
+      }, 5000);
+      setTimeout(() => {
+        observer.complete();            // it is either error or complete.. choose one. They cannot coexist
+      }, 7000);
+      setTimeout(() => {
+        observer.next('retry after complete');
+      }, 9000);
+    });
+
+    /* findings:
+     * - error message and next data must be separated in the subscribe()
+     * - error and complete will not exist both at the same time, it is only either one of them complete or error */
+    this.subscription2 = myObservable.subscribe(
+      (message: string) => {
+        console.log(message);
+      },
+      (errorMessage: string) => {
+        console.log(errorMessage);
+      },
+      () => {
+        console.log('the Observable is completed!');
+      }
+
+    );
+
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+    this.subscription2.unsubscribe();
   }
 
 }
